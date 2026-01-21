ac2681c9e2eda951536e95c69f9e1c0c4d62e86b
bohodays <bohodays0316@gmail.com>
2026-01-22 00:30:47 +0900

    (3.5) Zustand 미들웨어 2

--- onebite-exam/src/store/count.ts
+++ onebite-exam/src/store/count.ts
@@ -1,32 +1,67 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 /**
+ * middleware를 적용할때는 아래의 예시와 같은 순서로 적용해야 버그가 발생하지 않음
+
  * combine
  * - store 내부의 데이터와 action 함수를 각각 정의한 다음에 결합시켜주며, 타입스크립트의 추론 능력을 향상시킨다.
 
  * immer
  * - 객체가 중첩된 구조에서도 불변성 관리
+
+ * subscribeWithSelector
+ * - select 함수를 통해서 store의 특정 값을 구독하여 해당 값이 변경될때마다 특정 작업을 수행하도록 할 수 있음.
+
+ * persist
+ * - store의 데이터를 브라우저 스토리지에 저장하여 메모리 초기화 (새로고침) 시에도 데이터를 유지하도록 함
  */
 
 export const useCountStore = create(
-  immer(
-    // 타입을 정의하지 않아도 현재 store의 상태를 추론함
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increase: () => {
-          set((state) => {
-            state.count += 1;
-          });
-        },
-        decrease: () => {
-          set((state) => {
-            state.count -= 1;
-          });
-        },
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          // 타입을 정의하지 않아도 현재 store의 상태를 추론함
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
       },
-    })),
+    ),
+    { name: "countStore" },
   ),
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, preCount) => {
+    console.log({ count, preCount });
+  },
 );
 
 type Store = {
