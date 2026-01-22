996aa89d249fca91ac14db8bbac78d2cce9dc423
bohodays <bohodays0316@gmail.com>
2026-01-23 00:40:06 +0900

    (3.7) 투두리스트 기능 구현하기

--- onebite-exam/src/components/todo-list/todo-item.tsx
+++ onebite-exam/src/components/todo-list/todo-item.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Button } from "../ui/button";
+import type { Todo } from "@/types";
+import { useDeleteTodo } from "@/store/todos";
 
-const TodoItem = ({ id, contents }: { id: number; contents: string }) => {
+const TodoItem = ({ id, content }: Todo) => {
+  const deleteTodo = useDeleteTodo();
+
+  const handleDeleteTodo = () => {
+    deleteTodo(id);
+  };
+
   return (
     <div className="flex items-center justify-between border p-2">
-      {contents}
-      <Button variant={"destructive"}>삭제</Button>
+      {content}
+      <Button onClick={handleDeleteTodo} variant={"destructive"}>
+        삭제
+      </Button>
     </div>
   );
 };
