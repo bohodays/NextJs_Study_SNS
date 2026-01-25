8a18965e19ecff74c12acaeebd5015ee1f206213
bohodays <bohodays0316@gmail.com>
2026-01-25 14:26:05 +0900

    (4.3) 데이터 조회 요청 관리하기

--- onebite-exam/src/pages/todo-list-page.tsx
+++ onebite-exam/src/pages/todo-list-page.tsx
@@ -1,16 +1,19 @@
 import TodoEditor from "@/components/todo-list/todo-editor";
 import TodoItem from "@/components/todo-list/todo-item";
-import { useTodos } from "@/store/todos";
-import React from "react";
+import { useTodosData } from "@/hooks/queries/use-todos.data";
 
 const TodoListPage = () => {
-  const todos = useTodos();
+  const { data: todos, isLoading, error } = useTodosData();
+
+  if (error) return <div>오류가 발생했습니다.</div>;
+
+  if (isLoading) return <div>로딩 중 입니다 ...</div>;
 
   return (
     <div className="flex flex-col gap-5 p-5">
       <h1 className="text-2xl font-bold">TodoList</h1>
       <TodoEditor />
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
     </div>
