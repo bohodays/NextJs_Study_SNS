25cb871de60e2675112ddcb0605c22cc3812d06a
bohodays <bohodays0316@gmail.com>
2026-02-01 16:29:16 +0900

    (6.11) 회원가입시 프로필 정보 자동 생성하기

---
+++ onebite-log/src/api/profile.ts
@@ -0,0 +1,27 @@
+import supabase from "@/lib/supabase";
+import { getRandomNickname } from "@/lib/utils";
+
+export const fetchProfile = async (userId: string) => {
+  const { data, error } = await supabase
+    .from("profile")
+    .select("*")
+    .eq("id", userId)
+    .single();
+
+  if (error) throw error;
+  return data;
+};
+
+export const createProfile = async (userId: string) => {
+  const { data, error } = await supabase
+    .from("profile")
+    .insert({
+      id: userId,
+      nickname: getRandomNickname(),
+    })
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+};
