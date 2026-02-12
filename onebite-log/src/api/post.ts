c228bd5da72ae0ceeeb005b37628b69592c6ba18
bohodays <bohodays0316@gmail.com>
2026-02-12 21:40:10 +0900

    (8.2) 좋아요 기능 구현하기

--- onebite-log/src/api/post.ts
+++ onebite-log/src/api/post.ts
@@ -106,3 +106,19 @@ export const deletePost = async (id: number) => {
   if (error) throw error;
   return data;
 };
+
+export const togglePostLike = async ({
+  postId,
+  userId,
+}: {
+  postId: number;
+  userId: string;
+}) => {
+  const { data, error } = await supabase.rpc("toggle_post_like", {
+    p_post_id: postId,
+    p_user_id: userId,
+  });
+
+  if (error) throw error;
+  return data;
+};
