023d4cc87f50235236bbab238882799cc84df777
bohodays <bohodays0316@gmail.com>
2026-02-16 22:56:04 +0900

    (10.2) 댓글 추가 기능 구현하기

---
+++ onebite-log/src/api/comment.ts
@@ -0,0 +1,21 @@
+import supabase from "@/lib/supabase";
+
+export const createComment = async ({
+  postId,
+  content,
+}: {
+  postId: number;
+  content: string;
+}) => {
+  const { data, error } = await supabase
+    .from("comment")
+    .insert({
+      post_id: postId,
+      content,
+    })
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+};
--- onebite-log/src/pages/post-detail-page.tsx
+++ onebite-log/src/pages/post-detail-page.tsx
@@ -6,15 +6,15 @@ import { Navigate, useParams } from "react-router";
 
 const PostDetailPage = () => {
   const params = useParams();
-  const postId = params.postId;
+  const postId = Number(params.postId);
 
   if (!postId) return <Navigate to="/" />;
 
   return (
     <div className="flex flex-col gap-5">
-      <PostItem postId={Number(postId)} type="DETAIL" />
+      <PostItem postId={postId} type="DETAIL" />
       <div className="text-xl font-bold">댓글</div>
-      <CommentEditor />
+      <CommentEditor postId={postId} />
       <CommentList />
     </div>
   );
