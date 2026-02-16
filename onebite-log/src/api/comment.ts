51ddf869ff2856a34e799b82e9219fbc2b72d896
bohodays <bohodays0316@gmail.com>
2026-02-17 02:11:21 +0900

    (10.8) 무한 중첩 댓글 구현하기

--- onebite-log/src/api/comment.ts
+++ onebite-log/src/api/comment.ts
@@ -4,10 +4,12 @@ export const createComment = async ({
   postId,
   content,
   parentCommentId,
+  rootCommentId,
 }: {
   postId: number;
   content: string;
   parentCommentId?: number;
+  rootCommentId?: number;
 }) => {
   const { data, error } = await supabase
     .from("comment")
@@ -15,6 +17,7 @@ export const createComment = async ({
       post_id: postId,
       content,
       parent_comment_id: parentCommentId,
+      root_comment_id: rootCommentId,
     })
     .select()
     .single();
